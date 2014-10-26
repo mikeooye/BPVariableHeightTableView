72c88ab011f16ff20cb777c09387a79d71c8e06e
LiHaozhen <bp34@163.com>
2014-10-26 17:44:15 +0800

    创建项目

---
+++ BPVariableHeightTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BPVariableHeightTableView
+//
+//  Created by LiHaozhen on 14/10/26.
+//  Copyright (c) 2014年 ihojin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BPVariableHeightTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BPVariableHeightTableView
+//
+//  Created by LiHaozhen on 14/10/26.
+//  Copyright (c) 2014年 ihojin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
